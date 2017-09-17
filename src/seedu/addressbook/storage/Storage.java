bf33b0883ab8a2f7c79f1505da8f3b6ff2b9781b
Justin Poh <e0031499@u.nus.edu>
2017-09-17 23:34:56 +0800

    Altered StorageFile to now depend on Storage and try to match the demands of Storage.

--- src/seedu/addressbook/storage/Storage.java
+++ src/seedu/addressbook/storage/Storage.java
@@ -1,4 +1,38 @@
 package seedu.addressbook.storage;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
 
-public class Storage {
+/**
+ * An abstract class that defines the functionality of a storage
+ * for the AddressBook.
+ */
+
+public abstract class Storage {
+
+
+    /**
+     * Signals that something went wrong when attempting to initialize a storage.
+     */
+    public static class InvalidStorageException extends IllegalValueException {
+        public InvalidStorageException(String message){
+            super(message);
+        }
+    }
+
+    /**
+     * Signals that some error has occurred while trying to convert and read/write data between the application
+     * and the storage file.
+     */
+    public static class StorageOperationException extends Exception {
+        public StorageOperationException(String message) {
+            super(message);
+        }
+    }
+
+    public abstract void save(AddressBook addressbook) throws StorageOperationException;
+    public abstract AddressBook load() throws StorageOperationException;
+    public abstract String getPath();
+    public static Storage getStorage() throws InvalidStorageException {
+        return new StorageFile();
+    }
 }
